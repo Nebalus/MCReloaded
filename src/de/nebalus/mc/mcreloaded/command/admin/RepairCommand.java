0617d9568ca83eaec20aa25452548d8a7427f442
Nebalus <56486387+Nebalus@users.noreply.github.com>
2023-01-27 12:19:50 +0100

    Add files via upload

--- src/de/nebalus/mc/mcreloaded/command/admin/RepairCommand.java
+++ src/de/nebalus/mc/mcreloaded/command/admin/RepairCommand.java
@@ -2,6 +2,8 @@ package de.nebalus.mc.mcreloaded.command.admin;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 
 import de.nebalus.mc.mcreloaded.command.MCCommand;
 
@@ -11,6 +13,14 @@ public class RepairCommand extends MCCommand
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) 
 	{
+		if(!(sender instanceof Player))
+		{
+			sender.sendMessage("You need to be a player to execute this command!");
+			return false;
+		}
+		
+		Player p = (Player) sender;
+		
 		
 		return false;
 	}
